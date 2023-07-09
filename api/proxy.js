865af238a47c9e046d9d7848137f6b21cac97a0e
Huac233 <96987145+Huac233@users.noreply.github.com>
2023-07-09 17:21:08 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
   createProxyMiddleware({
-    target: "http://45.148.134.152:8000",
+    target: "https://api.xn--5gq551h.co",
     changeOrigin: true,
   })(req, res)
 }
